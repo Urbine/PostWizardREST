9810a05a82dd7bc1c00e6f99533460ff1c83da5d
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-08-14 16:26:54 +0700

    refactor(enums): ProcessingStrategy Javadoc formatting updated.

--- src/main/java/net/ygbstudio/postdirector/enums/ProcessingStrategy.java
+++ src/main/java/net/ygbstudio/postdirector/enums/ProcessingStrategy.java
@@ -2,14 +2,14 @@ package net.ygbstudio.postdirector.enums;
 
 /**
  * Enum representing the processing strategies for tasks in the PostDirector application.
- * 
- * This enum defines two strategies:
- * - PARALLEL: Tasks are processed concurrently, allowing for faster execution.
- * - SEQUENTIAL: Tasks are processed one after another, ensuring order of execution.
- * 
+ *
+ * <p>This enum defines two strategies: - PARALLEL: Tasks are processed concurrently, allowing for
+ * faster execution. - SEQUENTIAL: Tasks are processed one after another, ensuring order of
+ * execution.
+ *
  * @author Yoham Gabriel B @ YGB Studio
  */
 public enum ProcessingStrategy {
-	PARALLEL,
-	SEQUENTIAL,
+  PARALLEL,
+  SEQUENTIAL,
 }
