2481cbffce4df9ac6430a3e6ea42bf4ea30b175b
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-09-15 21:03:34 +0700

    feat(core): migrate ClientPost to camelCase fields and standardize JSON names & timezones
    ClientPost: renamed snake_case fields to camelCase (e.g., post_author -> postAuthor) and adjusted @JsonbProperty names and @JsonbPropertyOrder accordingly. Updated constructor parameter names and assignments. Also standardized GMT handling to ZoneOffset.UTC.

--- src/main/java/net/ygbstudio/postwizard/dto/ClientPost.java
+++ src/main/java/net/ygbstudio/postwizard/dto/ClientPost.java
@@ -5,7 +5,7 @@ import jakarta.json.bind.annotation.JsonbProperty;
 import jakarta.json.bind.annotation.JsonbPropertyOrder;
 import jakarta.json.bind.annotation.JsonbVisibility;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -24,19 +24,19 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 @JsonbPropertyOrder(
     value = {
       "ID",
-      "post_author",
-      "post_title",
-      "post_name",
-      "post_status",
-      "post_content",
-      "post_type",
+      "postAuthor",
+      "postTitle",
+      "postName",
+      "postStatus",
+      "postContent",
+      "postType",
       "guid",
-      "post_mime_type",
-      "post_parent",
-      "post_date",
-      "post_date_gmt",
-      "post_modified",
-      "post_modified_gmt"
+      "postMimeType",
+      "postParent",
+      "postDate",
+      "postDateGMT",
+      "postModified",
+      "postModifiedGMT"
     })
 @JsonbVisibility(ClientDTOVisibilityStrategy.class)
 public class ClientPost implements BatchDeliverable {
@@ -46,35 +46,35 @@ public class ClientPost implements BatchDeliverable {
 
   @JsonbNillable
   @JsonbProperty("author")
-  private Long post_author;
+  private Long postAuthor;
 
   @JsonbNillable
   @JsonbProperty("content")
-  private String post_content;
+  private String postContent;
 
   @JsonbNillable
   @JsonbProperty("title")
-  private String post_title;
+  private String postTitle;
 
   @JsonbNillable
   @JsonbProperty("slug")
-  private String post_name;
+  private String postName;
 
   @JsonbNillable
   @JsonbProperty("status")
-  private String post_status;
+  private String postStatus;
 
   @JsonbNillable
-  @JsonbProperty("postType")
-  private String post_type;
+  @JsonbProperty("type")
+  private String postType;
 
   @JsonbNillable
   @JsonbProperty("mimeType")
-  private String post_mime_type;
+  private String postMimeType;
 
   @JsonbNillable
   @JsonbProperty("postParent")
-  private Long post_parent;
+  private Long postParent;
 
   @JsonbNillable
   @JsonbProperty("guid")
@@ -82,19 +82,19 @@ public class ClientPost implements BatchDeliverable {
 
   @JsonbNillable
   @JsonbProperty("createdAt")
-  private LocalDateTime post_date;
+  private LocalDateTime postDate;
 
   @JsonbNillable
   @JsonbProperty("createdAtGMT")
-  private LocalDateTime post_date_gmt;
+  private LocalDateTime postDateGMT;
 
   @JsonbNillable
   @JsonbProperty("modifiedAt")
-  private LocalDateTime post_modified;
+  private LocalDateTime postModified;
 
   @JsonbNillable
   @JsonbProperty("modifiedAtGMT")
-  private LocalDateTime post_modified_gmt;
+  private LocalDateTime postModifiedGMT;
 
   public ClientPost() {
     super();
@@ -103,53 +103,51 @@ public class ClientPost implements BatchDeliverable {
   /**
    * Constructor for ClientPost.
    *
-   * <p>*
-   *
    * <p>Just like {@link ClientPostMeta}, the fields are expected to be optional and can be null.
    * postID can also be null when creating a new post, as it will be provided by the client in the
    * request path or in the corresponding payload.
    *
-   * <p>post_date_gmt is set to the current date and time if post_date is provided, while
-   * post_modified and post_modified_gmt are set to the current date and time by default in their
-   * respective zones.
+   * <p>postDateGMT is set to the current date and time if postDate is provided, while post_modified
+   * and post_modified_gmt are set to the current date and time by default in their respective
+   * zones.
    *
    * @param iD The unique identifier of the post.
-   * @param post_author The ID of the author of the post.
-   * @param post_content The content of the post.
-   * @param post_title The title of the post.
-   * @param post_name The slug (URL-friendly name) of the post.
-   * @param post_status The status of the post (e.g., published, draft).
-   * @param post_type The type of the post (e.g., post, page).
-   * @param post_mime_type The MIME type of the post.
-   * @param post_parent The ID of the parent post, if applicable.
+   * @param postAuthor The ID of the author of the post.
+   * @param postContent The content of the post.
+   * @param postTitle The title of the post.
+   * @param postName The slug (URL-friendly name) of the post.
+   * @param postStatus The status of the post (e.g., published, draft).
+   * @param postType The type of the post (e.g., post, page).
+   * @param postMimeType The MIME type of the post.
+   * @param postParent The ID of the parent post, if applicable.
    * @param guid The globally unique identifier for the post.
    */
   public ClientPost(
       Long iD,
-      Long post_author,
-      String post_content,
-      String post_title,
-      String post_name,
-      String post_status,
-      String post_type,
-      String post_mime_type,
-      Long post_parent,
+      Long postAuthor,
+      String postContent,
+      String postTitle,
+      String postName,
+      String postStatus,
+      String postType,
+      String postMimeType,
+      Long postParent,
       String guid) {
     super();
     ID = iD;
-    this.post_author = post_author;
-    this.post_content = post_content;
-    this.post_title = post_title;
-    this.post_name = post_name;
-    this.post_status = post_status;
-    this.post_type = post_type;
-    this.post_mime_type = post_mime_type;
-    this.post_parent = post_parent;
+    this.postAuthor = postAuthor;
+    this.postContent = postContent;
+    this.postTitle = postTitle;
+    this.postName = postName;
+    this.postStatus = postStatus;
+    this.postType = postType;
+    this.postMimeType = postMimeType;
+    this.postParent = postParent;
     this.guid = guid;
-    this.post_date = LocalDateTime.now();
-    this.post_date_gmt = ZonedDateTime.now(ZoneId.of("Etc/GMT-0")).toLocalDateTime();
-    this.post_modified = LocalDateTime.now();
-    this.post_modified_gmt = ZonedDateTime.now(ZoneId.of("Etc/GMT-0")).toLocalDateTime();
+    this.postDate = LocalDateTime.now();
+    this.postDateGMT = ZonedDateTime.now(ZoneOffset.UTC).toLocalDateTime();
+    this.postModified = LocalDateTime.now();
+    this.postModifiedGMT = ZonedDateTime.now(ZoneOffset.UTC).toLocalDateTime();
   }
 
   public Long getID() {
@@ -161,67 +159,67 @@ public class ClientPost implements BatchDeliverable {
   }
 
   public Long getAuthor() {
-    return post_author;
+    return postAuthor;
   }
 
   public void setAuthor(Long post_author) {
-    this.post_author = post_author;
+    this.postAuthor = post_author;
   }
 
   public String getContent() {
-    return post_content;
+    return postContent;
   }
 
   public void setContent(String post_content) {
-    this.post_content = post_content;
+    this.postContent = post_content;
   }
 
   public String getTitle() {
-    return post_title;
+    return postTitle;
   }
 
   public void setTitle(String post_title) {
-    this.post_title = post_title;
+    this.postTitle = post_title;
   }
 
   public String getSlug() {
-    return post_name;
+    return postName;
   }
 
   public void setSlug(String post_name) {
-    this.post_name = post_name;
+    this.postName = post_name;
   }
 
   public String getStatus() {
-    return post_status;
+    return postStatus;
   }
 
   public void setStatus(String post_status) {
-    this.post_status = post_status;
+    this.postStatus = post_status;
   }
 
   public String getType() {
-    return post_type;
+    return postType;
   }
 
   public void setType(String post_type) {
-    this.post_type = post_type;
+    this.postType = post_type;
   }
 
   public String getMimeType() {
-    return post_mime_type;
+    return postMimeType;
   }
 
   public void setMimeType(String post_mime_type) {
-    this.post_mime_type = post_mime_type;
+    this.postMimeType = post_mime_type;
   }
 
   public Long getParent() {
-    return post_parent;
+    return postParent;
   }
 
   public void setParent(Long post_parent) {
-    this.post_parent = post_parent;
+    this.postParent = post_parent;
   }
 
   public String getGuid() {
@@ -233,35 +231,35 @@ public class ClientPost implements BatchDeliverable {
   }
 
   public LocalDateTime getCreateDate() {
-    return post_date;
+    return postDate;
   }
 
   public void setCreateDate(LocalDateTime post_date) {
-    this.post_date = post_date;
+    this.postDate = post_date;
   }
 
   public LocalDateTime getCreateDateGMT() {
-    return post_date_gmt;
+    return postDateGMT;
   }
 
   public void setCreateDateGMT(LocalDateTime post_date_gmt) {
-    this.post_date_gmt = post_date_gmt;
+    this.postDateGMT = post_date_gmt;
   }
 
   public LocalDateTime getDateModified() {
-    return post_modified;
+    return postModified;
   }
 
   public void setDateModified(LocalDateTime post_modified) {
-    this.post_modified = post_modified;
+    this.postModified = post_modified;
   }
 
   public LocalDateTime getDateModifiedGMT() {
-    return post_modified_gmt;
+    return postModifiedGMT;
   }
 
   public void setDateModifiedGMT(LocalDateTime post_modified_gmt) {
-    this.post_modified_gmt = post_modified_gmt;
+    this.postModifiedGMT = post_modified_gmt;
   }
 
   @Override
