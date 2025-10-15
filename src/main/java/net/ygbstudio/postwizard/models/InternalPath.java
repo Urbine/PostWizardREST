20fe1c3ead8b3ab9292e24c66c9e9ee2aa5ee597
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-10-15 18:40:55 +0700

    feat(models): add InternalPath and SiteOption enums for WordPress configuration
    Created InternalPath enum for common WordPress URL paths (wp-content, uploads)
    Added SiteOption enum for WordPress option names (siteurl, home, blogname, etc.)

---
+++ src/main/java/net/ygbstudio/postwizard/models/InternalPath.java
@@ -0,0 +1,27 @@
+package net.ygbstudio.postwizard.models;
+
+/**
+ * Enum class that intends to model common URL paths within a WordPress site.
+ *
+ * <p>This class is useful to construct internal paths in a less error-prone manner.
+ *
+ * <p>This class is not exhaustive and more paths may be added in the future depending on the needs
+ * of the application.
+ *
+ * @author Yoham Gabriel B @ YGB Studio
+ */
+public enum InternalPath {
+  WP_CONTENT("wp-content"),
+  UPLOADS("uploads");
+
+  private final String value;
+
+  InternalPath(String value) {
+    this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    return value;
+  }
+}
---
+++ src/main/java/net/ygbstudio/postwizard/models/SiteOption.java
@@ -0,0 +1,32 @@
+package net.ygbstudio.postwizard.models;
+
+/**
+ * Represents WordPress options stored in the database.
+ *
+ * <p>Site options enum class for service-layer validation of site configuration in a WordPress
+ * installation.
+ *
+ * <p>This model is not exhaustive and more options may be added in the future.
+ *
+ * @see net.ygbstudio.postwizard.entities.WPOptions
+ * @author Yoham Gabriel @ YGB Studio
+ */
+public enum SiteOption {
+  SITE_URL("siteurl"),
+  HOME_URL("home"),
+  BLOG_NAME("blogname"),
+  BLOG_DESCRIPTION("blogdescription"),
+  ADMIN_EMAIL("admin_email"),
+  TIMEZONE_STRING("timezone_str");
+
+  private final String value;
+
+  SiteOption(String value) {
+    this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    return value;
+  }
+}
