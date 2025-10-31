bf5459c6b8e86be3c7ea8038c15d7d252f925df2
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-10-31 14:29:14 +0700

    refactor(dao/test): clean imports and remove unused DAO/test imports in tests
    Tests no longer import certain DAO classes that were unused after refactor (cleaned up test imports).

--- src/main/java/net/ygbstudio/postwizard/service/TaxonomyService.java
+++ src/main/java/net/ygbstudio/postwizard/service/TaxonomyService.java
@@ -27,7 +27,6 @@ import net.ygbstudio.postwizard.entities.taxonomies.WPTermTaxonomy;
 import net.ygbstudio.postwizard.entities.taxonomies.WPTerms;
 import net.ygbstudio.postwizard.models.Taxonomy;
 import net.ygbstudio.postwizard.models.TermMeta;
-import net.ygbstudio.postwizard.rest.PostController;
 import org.apache.commons.lang3.StringUtils;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
@@ -47,7 +46,7 @@ import org.jspecify.annotations.Nullable;
  * @see TermsMetaManager
  * @see TermRelationshipsManager
  * @see PostManager
- * @see PostController
+ * @see net.ygbstudio.postwizard.rest.TaxonomyController
  * @author Yoham Gabriel @ YGB Studio
  */
 @ApplicationScoped
